e1ec59853a714af8097641d8269e795e5416d223
QTuan <tuanlqhe173471@fpt.edu.vn>
2025-02-12 19:05:18 +0700

    [Features Authentication : Code login , forgot password , , authentication]

---
+++ frontend/src/store/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '@/store/auth-slice/authSlice';
+import cartReducer from './cart-slice/cartSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { combineReducers } from 'redux';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
