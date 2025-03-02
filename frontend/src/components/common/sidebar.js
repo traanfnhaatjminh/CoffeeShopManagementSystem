52d0161f4ddb1cc6059d7b6a8a23fd1cf0cabfe3
traanfnhaatjminh <minh12a2nvc@gmail.com>
2025-03-03 01:01:37 +0700

    update

--- frontend/src/components/common/sidebar.js
+++ frontend/src/components/common/sidebar.js
@@ -71,13 +71,6 @@ function Sidebar() {
                 <span className="text-sm text-gray-700 font-lauren font-semibold">Thông tin</span>
               </Link>
             </div>
-            <div className="abc">
-              <MdListAlt className="w-6 h-6 mb-1 text-gray-700" />
-
-              <Link to="/warehouse/profile">
-                <span className="text-sm text-gray-700 font-lauren font-semibold">Thông tin</span>
-              </Link>
-            </div>
           </>
         )}
 
