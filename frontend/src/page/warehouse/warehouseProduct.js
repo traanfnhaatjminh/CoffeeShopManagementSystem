58ed78e5af225e30269691b3c130c0b244d56a0d
traanfnhaatjminh <minh12a2nvc@gmail.com>
2025-03-09 21:27:08 +0700

    fix bug

--- frontend/src/page/warehouse/warehouseProduct.js
+++ frontend/src/page/warehouse/warehouseProduct.js
@@ -120,6 +120,9 @@ function WarehouseProduct() {
                     Tên đồ uống
                   </th>
                   <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Giá vốn 
+                  </th>
+                  <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
                     Giá bán
                   </th>
                   <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
@@ -142,19 +145,11 @@ function WarehouseProduct() {
                     <tr key={product._id} className="border-b hover:bg-gray-100 transition-colors duration-300">
                       <td className="px-6 py-4 text-lg font-medium text-gray-900"> {index + 1 + (currentPage - 1) * productPerPage}</td>
                       <td className="px-6 py-4 text-md text-gray-500">{product.pname}</td>
-                      {/*  {product.status === 1 && (
-                        <span className="text-green-500 ml-2">
-                          <FaCheck title="Sản phẩm khả dụng" />
-                        </span>
-                      )}
-                      {product.status === 0 && (
-                        <span className="text-red-500 ml-2">không khả dụng
-                          <MdCancel title="Sản phẩm không khả dụng" />
-                        </span>
-                      )}</td>*/}
+                      <td className="px-6 py-4 text-md text-gray-500">
+                        {new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(product.cost_price)}
+                      </td>
                       <td className="px-6 py-4 text-md text-gray-500">
                       {new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(product.sale_price)}
-      
                       </td>
                       <td className="px-6 py-4 text-md text-gray-500">
                         <img src={product.image} alt={product.pname} className="w-16 h-16 object-cover rounded-lg" />
